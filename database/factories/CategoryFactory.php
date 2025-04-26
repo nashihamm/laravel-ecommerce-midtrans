7229e6fae65508469cb2d5cfff9a91d7ccffca0b
nashihamm <nashihamm@gmail.com>
2025-04-26 19:39:38 +0700

    add CategoryFactory for generating category test data

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->words(rand(1, 2), true);
+        $slug = Str::slug($name);
+        
+        return [
+            'name' => ucwords($name),
+            'slug' => $slug,
+        ];
+    }
+}
